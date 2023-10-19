7d06965d9ed074e71f7403234141da1959edd073
sinarkageek <bonjour@sinarka34.com>
2023-10-19 23:56:15 +0200

    test api plats

--- pages/index.js
+++ pages/index.js
@@ -25,30 +25,6 @@ export default function Home() {
             <p> Voir les endpoints de strapi en action </p>
           </a>
 
-          <a href="" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href=""
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
--- pages/test/index.js
+++ pages/test/index.js
@@ -1,63 +1,109 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+import styles from '../../styles/Home.module.css';
 
-export default function App() {
-  const [userCount, setUserCount] = useState(0);
+export default function Home() {
   const [plats, setPlats] = useState([]);
+  const [newDish, setNewDish] = useState({ titre: '', ingredients: [] });
   const [ingredients, setIngredients] = useState([]);
-  const [pageData, setPageData] = useState(null);
 
+  // Chargement initial des données
   useEffect(() => {
-    // Comptage des utilisateurs
-    axios.get('https://strapi.furamingo.app/api/users/count')
-      .then(response => {
-        setUserCount(response.data);
-      })
-      .catch(error => {
-        console.error('Erreur lors du comptage des utilisateurs:', error);
-      });
+    const fetchData = async () => {
+      try {
+        const [responsePlats, responseIngredients] = await Promise.all([
+          axios.get('https://strapi.furamingo.app/api/plats?populate=*'),
+          axios.get('https://strapi.furamingo.app/api/ingredients')
+        ]);
 
-    // Obtention des plats
-    axios.get('https://strapi.furamingo.app/api/plats')
-      .then(response => {
-        setPlats(response.data);
-      })
-      .catch(error => {
-        console.error('Erreur lors de la récupération des plats:', error);
-      });
+        setPlats(responsePlats.data.data || []);
+        setIngredients(responseIngredients.data.data || []);
+      } catch (error) {
+        console.error('Erreur lors du chargement des données:', error);
+      }
+    };
 
-    // Obtention des ingrédients
-    axios.get('https://strapi.furamingo.app/api/ingredients')
-      .then(response => {
-        setIngredients(response.data);
-      })
-      .catch(error => {
-        console.error('Erreur lors de la récupération des ingrédients:', error);
-      });
-
-    // Obtention des données de la page
-    axios.get('https://strapi.furamingo.app/api/page')
-      .then(response => {
-        setPageData(response.data);
-      })
-      .catch(error => {
-        console.error('Erreur lors de la récupération des données de la page:', error);
-      });
-
+    fetchData();
   }, []);
 
+  const handleInputChange = (e) => {
+    setNewDish({ ...newDish, titre: e.target.value });
+  };
+
+  const handleIngredientClick = (ingredientTitle) => {
+    if (!newDish.ingredients.includes(ingredientTitle)) {
+      setNewDish({ ...newDish, ingredients: [...newDish.ingredients, ingredientTitle] });
+    }
+  };
+
+  const handleAddDish = async () => {
+    try {
+      const response = await axios.post('https://strapi.furamingo.app/api/plats', newDish);
+      if (response.status === 200 || response.status === 201) {
+        setPlats([...plats, response.data]);
+      }
+    } catch (error) {
+      console.error('Erreur lors de l\'ajout d\'un nouveau plat:', error);
+    }
+  };
+
   return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">
-          User Count: {userCount}
-        </h1>
-        <div className="grid">
-          {/* Affichez ici vos plats, ingrédients, etc. */}
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Titres des plats et ingrédients</h1>
+        <div className={styles.grid}>
+          {/* Affichage des plats */}
+          {plats.map((plat, index) => (
+            <div key={index} className={styles.card}>
+              <h3 className={styles['card h3']}>{plat.attributes.titre}</h3>
+              <p className={styles['card p']}>Ingrédients :</p>
+              <ul>
+                {plat.attributes.ingredients?.data ? (
+                  plat.attributes.ingredients.data.map((ingredient, i) => (
+                    <li key={i}>{ingredient.attributes.titre}</li>
+                  ))
+                ) : (
+                  <li>Pas d'ingrédients disponibles</li>
+                )}
+              </ul>
+            </div>
+          ))}
+
+          {/* Formulaire d'ajout d'un nouveau plat */}
+          <div className={styles.card}>
+            <h3 className={styles['card h3']}>Ajouter un plat</h3>
+            <input
+              type="text"
+              placeholder="Titre du plat"
+              value={newDish.titre}
+              onChange={handleInputChange}
+              className={styles.input}
+            />
+            <p className={styles['card p']}>Ingrédients :</p>
+            {/* Liste des boutons pour ajouter des ingrédients */}
+            {ingredients.map((ingredient, index) => (
+              <button
+                key={index}
+                onClick={() => handleIngredientClick(ingredient.attributes.titre)}
+              >
+                {ingredient.attributes.titre}
+              </button>
+            ))}
+            {/* Liste des ingrédients ajoutés */}
+            <ul>
+              {newDish.ingredients.map((ingredient, i) => (
+                <li key={i}>{ingredient}</li>
+              ))}
+            </ul>
+            <button onClick={handleAddDish}>Ajouter</button>
+          </div>
         </div>
       </main>
-      <footer className="footer">
-        {/* Votre pied de page */}
+
+      <footer className={styles.footer}>
+        <a className={styles['footer a']}>
+          Powered by <img src="/logo.png" alt="Logo" className={styles['footer img']} />
+        </a>
       </footer>
     </div>
   );
